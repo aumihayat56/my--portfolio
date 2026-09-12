6871fedd9b0249d96801d66136a5ecd47f85956b
aumihayat56 <githayat7@gmail.com>
2026-09-12 11:28:43 +0300

    Refactor server.js for static files and listening
    Updated static file serving and changed listen address.

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(__dirname));
+
+
 app.use("/img", express.static(path.join(__dirname, "img")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`Portfolio running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Portfolio running on port ${PORT}`);
 });
